49225930b4224807f9a3e6f64fd5de011014fc53
jeandylan <dylanblais1@gmail.com>
2016-03-21 02:53:22 +0400

    idiodio

--- Sprite.js
+++ Sprite.js
@@ -8,3 +8,4 @@ canvas.height = 1000;
 //};
 var footBall = new BallSprite('image/ball3.svg', 800, 0, 800, 800, canvas.width / 2 - 400 / 2,  canvas.height / 2 - 400 / 2, 400, 400);
 footBall.draw(footBall, canvas);
+var frw=document
